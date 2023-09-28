b76438b37ca64c95723c5947c90bbad58e37df17
Rayan Al Mohaize <rayan.almohaize@gmail.com>
2023-09-28 15:53:38 +0200

    ajout du dossier ide avec le code

---
+++ projet_bdda_rayan_alex_djibril_adam/CODE/IDE_PROJET_rayan_alex_djibril_adam/src/couche_acces_disque/DBParams.java
@@ -0,0 +1,9 @@
+package couche_acces_disque;
+
+public class DBParams {
+	
+	public static String DBPath;
+	public static int SGBDSGBDPageSize;
+	public static int DMFileCount;
+
+}
---
+++ projet_bdda_rayan_alex_djibril_adam/CODE/IDE_PROJET_rayan_alex_djibril_adam/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author rayanalmohaize
+ *
+ */
+module IDE_PROJET_rayan_alex_djibril_adam {
+}
