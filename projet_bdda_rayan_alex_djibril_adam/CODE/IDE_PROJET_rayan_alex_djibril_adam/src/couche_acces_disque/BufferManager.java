f831c60cda01369665f48cbdbd9b1499725a202a
Rayan Al Mohaize <rayan.almohaize@gmail.com>
2023-10-12 15:42:18 +0200

    modif BufferManager

--- projet_bdda_rayan_alex_djibril_adam/CODE/IDE_PROJET_rayan_alex_djibril_adam/src/couche_acces_disque/BufferManager.java
+++ projet_bdda_rayan_alex_djibril_adam/CODE/IDE_PROJET_rayan_alex_djibril_adam/src/couche_acces_disque/BufferManager.java
@@ -1,5 +1,6 @@
 package couche_acces_disque;
 
+import java.nio.*;
 import java.util.*;
 
 public class BufferManager {
@@ -10,6 +11,35 @@ public class BufferManager {
 		this.listFrame = new ArrayList<Frame>();
 		for(int i = 0;i<DBParams.FrameCount;i++) {
 			listFrame.add(new Frame());
+		}
+	}
+	
+	public ByteBuffer getPage(PageId pageId) {
+		
+	}
+	
+	public void freePage(PageId pageId, int valdirty) {
+		for(int i = 0; i<listFrame.size();i++) {
+			if(listFrame.get(i).getPageId().equals(pageId)) {
+				if(listFrame.get(i).getPageId()!=null && listFrame.get(i).getPin_count() == 0) {
+					System.out.println("La page est déjà libre");
+				}
+				else {
+					Frame framefree = listFrame.get(i);
+					framefree.setPin_count(framefree.getPin_count()-1);
+					framefree.setFlag_dirty(valdirty);
+				}
+			}
+
+		}
+		System.out.println("Erreur, page non trouvée dans le BufferManager");
+	}
+	
+	public void flushAll() {
+		for(int i = 0; i<listFrame.size();i++) {
+			if(listFrame.get(i).getFlag_dirty() == 1) {
+				DiskManager.writePage(listFrame.get(i).getPageId(), listFrame.get(i).getBuffer());
+			}
 		}
 	}
 
